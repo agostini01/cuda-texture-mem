2a3b45cae93f8431a09f416d114c492bfb3f12c6
Nicolas Agostini <n.b.agostini@gmail.com>
2020-08-08 02:15:54 -0400

    Change channel type to be float

--- tex-obj-3d/main.cu
+++ tex-obj-3d/main.cu
@@ -7,40 +7,44 @@
 #include <iostream>
 #include <iomanip>
 
-void printArrayAsCharMatrix(const float* in, const size_t& width, const size_t& height) {
-    std::cout << std::endl;
-    char buffer[4];
-    int ret;
-    for (size_t j = 0; j < height; ++j)
+void printArrayAsCharMatrix(const float *in, const size_t &width, const size_t &height)
+{
+  std::cout << std::endl;
+  char buffer[4];
+  int ret;
+  for (size_t j = 0; j < height; ++j)
+  {
+    for (size_t i = 0; i < width; ++i)
     {
-      for (size_t i = 0; i < width; ++i)
-      {
-        ret=snprintf(buffer, sizeof buffer, "%f", in[width*j + i]);
+      ret = snprintf(buffer, sizeof buffer, "%f", in[width * j + i]);
 
-        // if (ret < 0) {
-        //   return EXIT_FAILURE;
-        // }
-        // if (ret >= sizeof buffer) {
-        // }
-        
-        std::cout << buffer[0]
-                  << buffer[1]
-                  << buffer[2]
-                  << buffer[3]
-                  << ' ';
-      }
-      std::cout << std::endl;
+      // if (ret < 0) {
+      //   return EXIT_FAILURE;
+      // }
+      // if (ret >= sizeof buffer) {
+      // }
+
+      std::cout << buffer[0] <<","
+                << buffer[1] <<","
+                << buffer[2] <<","
+                << buffer[3]
+                << ' ';
+    }
+    std::cout << std::endl;
   }
 }
 
-void printArrayAsMatrix(const float* in, const size_t& width, const size_t& height) {
-    std::cout << std::endl;
-  for (size_t j = 0; j < height; ++j) {
-    for (size_t i = 0; i < width; ++i) {
-      std::cout <<std::fixed 
-        << std::setw(5) // space between numbers
-        << std::setprecision(2) // nubmers after decimal point
-        << in[width*j + i] << ' ';
+void printArrayAsMatrix(const float *in, const size_t &width, const size_t &height)
+{
+  std::cout << std::endl;
+  for (size_t j = 0; j < height; ++j)
+  {
+    for (size_t i = 0; i < width; ++i)
+    {
+      std::cout << std::fixed
+                << std::setw(5)         // space between numbers
+                << std::setprecision(2) // nubmers after decimal point
+                << in[width * j + i] << ' ';
     }
     std::cout << std::endl;
   }
@@ -48,38 +52,41 @@ void printArrayAsMatrix(const float* in, const size_t& width, const size_t& heig
 
 __global__ void copyKernel(cudaSurfaceObject_t inputSurfObj,
                            cudaSurfaceObject_t outputSurfObj,
-                           int width, int height) 
+                           int width, int height)
 {
-    // Calculate surface coordinates
-    unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
-    unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
-    unsigned int idx = y * width + x;
+  // Calculate surface coordinates
+  unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+  unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+  unsigned int idx = y * width + x;
 
-    if (x < width && y < height) {
-        uchar4 data;
-        // Read from input surface
-        surf2Dread(&data,  inputSurfObj, x * 4, y);
-        if(idx==100) printf("%d\n", data.w);
-        // Write to output surface
-        data.w+=5;
-        surf2Dwrite(data, outputSurfObj, x * 4, y);
-    }
+  if (x < width && y < height)
+  {
+    float data;
+    // Read from input surface
+    surf2Dread(&data, inputSurfObj, x * 4, y);
+    if (idx == 100)
+      printf("%f\n", data);
+    // Write to output surface
+    data += 2;
+    surf2Dwrite(data, outputSurfObj, x * 4, y);
+  }
 }
 
-int main () 
+int main()
 {
   // Inputs
   size_t width = 16;
   size_t height = 16;
   size_t size = width * height * sizeof(float);
 
-  // Initialize host array 
-  float * h_data = (float*)malloc(size);
-  for (int i =0; i<height*width; ++i) h_data[i] =(float)i/(height*width);
+  // Initialize host array
+  float *h_data = (float *)malloc(size);
+  for (int i = 0; i < height * width; ++i)
+    h_data[i] = (float)i / (height * width);
 
   cudaChannelFormatDesc channelDesc =
-      cudaCreateChannelDesc(8, 8, 8, 8,
-                            cudaChannelFormatKindUnsigned);
+      cudaCreateChannelDesc(32, 0, 0, 0,
+                            cudaChannelFormatKindFloat);
 
   cudaArray *cuInputArray;
   cudaMallocArray(&cuInputArray, &channelDesc, width, height,
@@ -91,7 +98,6 @@ int main ()
 
   // checkCudaErrors(cudaMemcpyToArray(cu_array, 0, 0, h_data, size,
   //     cudaMemcpyHostToDevice));
-
 
   // Copy to device memory some data located at address h_data
   // in host memory
@@ -113,7 +119,7 @@ int main ()
   cudaCreateSurfaceObject(&outputSurfObj, &resDesc);
 
   // Allocate output buffer in device memory
-  float* d_output;
+  float *d_output;
   checkCudaErrors(cudaMalloc(&d_output, size));
   // Print result array
   // checkCudaErrors(cudaMemcpyToArray(cu_array, 0, 0, d_output, size,
@@ -122,26 +128,31 @@ int main ()
   //     cudaMemcpyDeviceToDevice));
   // checkCudaErrors(cudaMemcpy(h_data, d_output, size, cudaMemcpyDeviceToHost));
   checkCudaErrors(cudaMemcpyFromArray(h_data, cuInputArray, 0, 0, size,
-      cudaMemcpyDeviceToHost));
-  // printArrayAsMatrix(h_data, width, height);
-  printArrayAsCharMatrix(h_data, width, height);
-
+                                      cudaMemcpyDeviceToHost));
+  printArrayAsMatrix(h_data, width, height);
+  // printArrayAsCharMatrix(h_data, width, height);
 
   // Invoke kernel
   dim3 dimBlock(16, 16);
-  dim3 dimGrid((width  + dimBlock.x - 1) / dimBlock.x,
-      (height + dimBlock.y - 1) / dimBlock.y);
+  dim3 dimGrid((width + dimBlock.x - 1) / dimBlock.x,
+               (height + dimBlock.y - 1) / dimBlock.y);
+  // Copy from original surface and add 2
   copyKernel<<<dimGrid, dimBlock>>>(inputSurfObj,
                                     outputSurfObj,
                                     width, height);
-  
+
+  // Copy for the output surface and add another 2
+  copyKernel<<<dimGrid, dimBlock>>>(outputSurfObj,
+                                    outputSurfObj,
+                                    width, height);
+
   // checkCudaErrors(cudaMemcpyFromArray(d_output, cuOutputArray, 0, 0, size,
   //     cudaMemcpyDeviceToDevice));
   // checkCudaErrors(cudaMemcpy(h_data, d_output, size, cudaMemcpyDeviceToHost));
   checkCudaErrors(cudaMemcpyFromArray(h_data, cuOutputArray, 0, 0, size,
-      cudaMemcpyDeviceToHost));
-  // printArrayAsMatrix(h_data, width, height);
-  printArrayAsCharMatrix(h_data, width, height);
+                                      cudaMemcpyDeviceToHost));
+  printArrayAsMatrix(h_data, width, height);
+  // printArrayAsCharMatrix(h_data, width, height);
 
   // Destroy surface objects
   cudaDestroySurfaceObject(inputSurfObj);
@@ -157,4 +168,3 @@ int main ()
 
   return 0;
 }
-
